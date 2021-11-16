a96304be043911d9c444b01e7e41d42339f1f2ae
RDPolarity <rdpolarity@gmail.com>
2021-11-16 13:17:40 +1100

    Architecture setup

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './pages/Home';
+
+ReactDOM.render(
+  <React.StrictMode>
+      <h1>Test</h1>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.scss';
+
+function Home() {
+  return (
+    <div className="App">
+      <h1>Aiden Mellor</h1>
+    </div>
+  );
+}
+
+export default Home;
